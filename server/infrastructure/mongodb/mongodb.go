ab1220d32af268853decf6deca2bb44754e9d555
dominikus1993 <dominikus1910@gmail.com>
2020-08-08 13:51:04 +0200

    add client factory function

--- server/infrastructure/mongodb/mongodb.go
+++ server/infrastructure/mongodb/mongodb.go
@@ -1,9 +1,31 @@
 package mongodb
 
 import (
+	"context"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func NewClient() *mongo.Client {
+func NewClient(connectionString string) *mongo.Client {
 
+	// Set client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
 }
